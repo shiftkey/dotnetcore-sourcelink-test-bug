4ec68eb8d5e8ee49cb9baa3b6932e2bd6a66e4d2
Brendan Forster <github@brendanforster.com>
2020-01-29 15:32:46 -0400

    install sourcelink manually, update repro

--- repro.sh
+++ repro.sh
@@ -1,3 +1,4 @@
-dotnet tool restore
+git clean -xdf tools
+dotnet tool install sourcelink --tool-path=tools
 dotnet build -c Release
-dotnet sourcelink test bin/Release/netstandard2.0/dotnetcore-sourcelink-test-bug.dll
+./tools/sourcelink test bin/Release/netstandard2.0/dotnetcore-sourcelink-test-bug.dll
